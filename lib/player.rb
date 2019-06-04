686a12b4ba62671f144650c83636c4e9e73b7ec1
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-06-04 09:16:12 +0200

    Refactor and clean Player class.

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,5 @@
 class Player
 	attr_reader :color
-	# @@count = 0
-	
-	# def initialize
-	# 	@@count += 1
-	# 	@color = (@@count == 1 ? "White" : "Black")
-	# end
 
 	def initialize(color)
 		@color = color
